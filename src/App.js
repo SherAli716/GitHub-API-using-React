5288b6a4d220bfc79b75971a4e7c39654272d0f7
SherAli Shaikh <shaikhsherali81z@gmail.com>
2021-06-16 11:24:44 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//react-router
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+//toast
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+
+//firebase
+import firebase from "firebase/app";
+import "firebase/auth";
+
+//components
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import PageNotFound from "./Pages/PageNotFound";
+
+import { UserContext } from "./Context/UserContext";
+
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import FirebaseConfig from "./Config/FirebaseConfig";
+
+
+//init firebase
+firebase.initializeApp(FirebaseConfig)
+
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <Router>
+      <ToastContainer />
+      <UserContext.Provider value={{ user, setUser }}>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="*" component={PageNotFound} />
+        </Switch>
+        <Footer />
+      </UserContext.Provider>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/Config/FirebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyARShTivzsktANsOVIsxDcgKBjZDSMEavs",
+    authDomain: "mygitapp-97951.firebaseapp.com",
+    projectId: "mygitapp-97951",
+    storageBucket: "mygitapp-97951.appspot.com",
+    messagingSenderId: "749384703966",
+    appId: "1:749384703966:web:f414da983f1ce27e8d874f",
+    measurementId: "G-ZG0KQ3D7H9"
+  };
